1105adea7adb7b1df28ac2b01bc5ae085678b928
Raihan <Raihan jishan@gmail.com>
2022-11-21 20:08:07 +0600

    first commit

---
+++ .vscode/test.rs
@@ -0,0 +1,21 @@
+<div class="row">
+<div class="phoneOther">
+  <ul>
+    <li> <i class="fa fa-phone"></i>
+     </li>
+     <li>phone</li>
+
+    
+    <li> <i class="fa fa-envelope"></i></li>
+    <li>email</li>
+  </ul>
+  
+</div>
+
+<!--  socia media icons -->
+<div class="socialIcons">
+  <i class="fa fa-facebook"></i>
+  <i class="fa fa-twitter"></i>
+  <i class='fa fa-linkedin-square'></i>
+</div>
+</div>
---
+++ README.md
@@ -0,0 +1,6 @@
+#Briagte landing page
+project detailt ⤵
+! Trying to make constructional webpage using html css using fontAwesome icons. 
+Technology
+1. Html , [HyperTextMerkupLanguage]
+2. Css  , [cascadiStyleSheet]
